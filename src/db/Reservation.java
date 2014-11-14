d2c1562cbc2b36d93e3842c281587ce8ba135abe
Sondre <sondre.havellen@gmail.com>
2014-11-14 17:07:15 +0100

    Javadoc for the objects in db.

--- src/db/Reservation.java
+++ src/db/Reservation.java
@@ -2,7 +2,10 @@ package db;
 
 import org.joda.time.DateTime;
 
-
+/**
+ * This class contains the Reservation-class. It uses the class DateTime from the library JodaTime.
+ * @author Adrian Hundseth
+ */
 public class Reservation {
     private final int num_persons;
     private final DateTime date_to;
@@ -25,34 +28,66 @@ public class Reservation {
         this.koie_id = koie_id;
     }
 
+    /**
+     * This function returns the number of persons for the current reservation.
+     * @return Number of persons in the reservation.
+     */
     public int getNum_persons() {
         return num_persons;
     }
 
+    /**
+     * This function returns the end date of the reservation.
+     * @return The end date of the reservation.
+     */
     public DateTime getDate_to() {
         return date_to;
     }
 
+    /**
+     * This function returns a formatted version of the end date of the reservation.
+     * @return A formatted version of the end date of the reservation.
+     */
     public String getDate_to_formatted() {
 		return date_to_formatted;
 	}
 
+    /**
+     * This function returns the start date of the reservation.
+     * @return The start date of the reservation.
+     */
 	public DateTime getDate_from() {
         return date_from;
     }
 
+    /**
+     * This function returns a formatted version of the start date of the reservation.
+     * @return A formatted version of the start date of the reservation.
+     */
     public String getDate_from_formatted() {
 		return date_from_formatted;
 	}
 
+    /**
+     * This function returns the email of the reservation holder.
+     * @return The email of the reservation holder.
+     */
 	public String getEmail() {
         return email;
     }
 
+    /**
+     * This function returns the unique id of the reservation.
+     * @return The reservation id.
+     */
     public int getReservation_id() {
         return reservation_id;
     }
 
+    /**
+     * This function returns the id for the cabin for the current reservation.
+     * @return The id for the cabin for the current reservation.
+     */
     public int getKoie_id() {
         return koie_id;
     }
